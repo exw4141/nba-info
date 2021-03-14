a8c97d3e0cbb0677cd8ce00e8844f82fd65dc433
Edward Wong <exw4141@rit.edu>
2021-03-14 00:38:10 -0500

    Added comments

--- sites/espn_nba_players.py
+++ sites/espn_nba_players.py
@@ -5,9 +5,19 @@ from selenium.webdriver.common.by import By
 import time
 
 def open_nba_players_page(driver):
+    """
+    Open ESPN's NBA player statistics page
+
+    :param driver: Selenium WebDriver object
+    """
     driver.get('https://www.espn.com/nba/stats/player/_/table/general/sort/avgMinutes/dir/desc')
 
 def get_player_links(driver):
+    """
+    Get hyperlinks for all players on the player page
+
+    :param driver: Selenium WebDriver object
+    """
     current_url = driver.current_url
 
     if current_url == 'https://www.espn.com/nba/stats/player/_/table/general/sort/avgMinutes/dir/desc':
@@ -22,8 +32,6 @@ def process_player_game_log(driver):
     if current_url.startswith('https://www.espn.com/nba/player/_/id/'):
         game_log_tab = driver.find_element_by_link_text('Game Log')
         game_log_tab.click()
-        
-        
     else:
         print("Wrong webpage in use. Unable to extract player's game log")
 
